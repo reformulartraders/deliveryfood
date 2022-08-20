160fa55b9089f3ef530a8924b5d19651b863cb6d
reformulartraders <103069635+reformulartraders@users.noreply.github.com>
2022-08-19 23:30:07 -0300

    Update firebase.js

--- service/firebase.js
+++ service/firebase.js
@@ -1,11 +1,21 @@
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import { apiKey, projectId, authDomain } from '~/config'
-if (!firebase.apps.length) {
-  firebase.initializeApp({ apiKey, projectId, authDomain })
-}
-let db = firebase.firestore()
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-const googleAuthProvider = new firebase.auth.GoogleAuthProvider()
-export { db, firebase, googleAuthProvider }
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAy4VqaAzdBT9rVbdybefSeJPiljYKdw1g",
+  authDomain: "ording-83e6b.firebaseapp.com",
+  projectId: "ording-83e6b",
+  storageBucket: "ording-83e6b.appspot.com",
+  messagingSenderId: "597519792518",
+  appId: "1:597519792518:web:3b275640d210dce05a9407",
+  measurementId: "G-594DLHZ28S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
